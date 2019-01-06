0cd2c7ec2773a0624ab4c413f395f64602a3e45c
Shiqing <shiqing-thu18@yandex.com>
2019-01-06 23:41:33 +0800

    Tweak string literal display

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const defaultColors = ['23AC69', '91C132', 'F19726', 'E8552D', '1AAB8E', 'E1147F
 const defaultLanguageOptions = [
   /[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/,
   /(?:(?:^|\s)\/\/.+?$)|(?:\/\*[\S\s]*?\*\/)/,
-  /".*"|'.*'/
+  /"[\S\s]*?"|'[\S\s]*?'/
 ]
 
 const languageOptions = {
   'js': [null, null, null],
   'html': [null, /(?:<!--[\S\s]*?-->)/, null],
-  'py': [null, /(?:(?:^|\s)#.+?$)/, null],
+  'py': [null, /(?:(?:^|\s)#.+?$)/, /'''[\S\s]*?'''|"[\S\s]*?"|'[\S\s]*?'/],
   'go': [null, null, null],
   'hs': [/[\w|']+/, /(?:(?:^|\s)--.+?$)|(?:\{-[\S\s]*?-\})/, null]
 }
@@ -46,12 +46,16 @@ export default function (input, {
   const stringLitRe = options[2]
   const escapeCharRe = /[&<"']/
 
-  const re = new RegExp(`(${commentRe.source})|(${wordRe.source})|(${escapeCharRe.source})`, 'gmi')
+  const re = new RegExp(`(${commentRe.source})|(${stringLitRe.source})|(${wordRe.source})|(${escapeCharRe.source})`, 'gmi')
   console.log(re)
   return input
-  .replace(re, (m, cm, word, esc) => {
+  .replace(re, (m, cm, str, word, esc) => {
     if (cm) {
       return toComment(cm)
+    }
+
+    if (str) {
+      return toStringLit(str)
     }
 
     if (esc) {
@@ -74,4 +78,8 @@ export default function (input, {
 
 function toComment(cm) {
   return `<span style="color: slategray">${escapeHTML(cm)}</span>`
+}
+
+function toStringLit(cm) {
+  return `<span style="color: mediumslateblue">${escapeHTML(cm)}</span>`
 }
