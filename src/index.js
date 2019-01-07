ac26ad82790c265aa69b70afb25ce2b93b386bf4
Shiqing <shiqing-thu18@yandex.com>
2019-01-07 11:58:24 +0800

    Chroma!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import jsCommentRe from 'comment-regex'
 
-const defaultColors = ['23AC69', '91C132', 'F19726', 'E8552D', '1AAB8E', 'E1147F', '2980C1', '1BA1E6', '9FA0A0', 'F19726', 'E30B20', 'E30B20', 'A3338B']
+// const defaultColors = Array.from(new Array(18), (_, i) => [i * 20, 80, 50])
+const defaultColors = [
+  [340, 80, 65],
+  [200, 80, 45],
+  [120, 80, 40],
+  [300, 80, 55],
+  [260, 80, 60],
+  [140, 80, 40],
+  [60, 100, 39],
+  [0, 75, 65],
+  [100, 80, 40],
+  [80, 80, 45],
+  [240, 80, 70],
+  [20, 90, 55],
+  [180, 80, 40],
+  [280, 80, 55],
+  [160, 80, 40],
+  [320, 80, 60],
+  [220, 80, 55],
+  [40, 100, 48]
+]
 
 const defaultLanguageOptions = [
   /[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/,
@@ -53,16 +73,24 @@ class ColorCache {
   }
 }
 
+const saturate = function (c, rate) {
+  return [c[0], c[1] * rate, c[2]]
+}
+
+const cssColor = function (c) {
+  return `hsl(${c[0]}, ${c[1]}%, ${c[2]}%)`
+}
+
 const colorizeComment = function (cm) {
   return `<span style="color: slategray">${escapeHTML(cm)}</span>`
 }
 
 const colorizeStrLit = function (cache, str) {
-  return `<span style="color: #${cache.forWord(str)}; filter: saturate(0.4)">${escapeHTML(str)}</span>`
+  return `<span style="color: ${cssColor(saturate(cache.forWord(str), 0.4))}">${escapeHTML(str)}</span>`
 }
 
 const colorizeWord = function (cache, word) {
-  return `<span style="color: #${cache.forWord(word)}">${word}</span>`
+  return `<span style="color: ${cssColor(cache.forWord(word))}">${word}</span>`
 }
 
 export default function (input, {
