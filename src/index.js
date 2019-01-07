41b586e98b2331bcd25099c617a1da8b4f6d9167
Shiqing <shiqing-thu18@yandex.com>
2019-01-07 10:47:21 +0800

    A little cleanup

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const defaultColors = ['23AC69', '91C132', 'F19726', 'E8552D', '1AAB8E', 'E1147F
 
 const defaultLanguageOptions = [
   /[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/,
-  /(?:(?:^|\s)\/\/.+?$)|(?:\/\*[\S\s]*?\*\/)/,
+  /(?:^|\s)\/\/.+?$|\/\*[\S\s]*?\*\//,
   /"[\S\s]*?"|'[\S\s]*?'/
 ]
 
 const languageOptions = {
   'js': [null, null, null],
-  'html': [null, /(?:<!--[\S\s]*?-->)/, null],
-  'py': [null, /(?:(?:^|\s)#.+?$)/, /'''[\S\s]*?'''|"[\S\s]*?"|'[\S\s]*?'/],
+  'html': [null, /<!--[\S\s]*?-->/, null],
+  'py': [null, /(?:^|\s)#.+?$/, /'''[\S\s]*?'''|"[\S\s]*?"|'[\S\s]*?'/],
   'go': [null, null, null],
-  'hs': [/[\w|']+/, /(?:(?:^|\s)--.+?$)|(?:\{-[\S\s]*?-\})/, null]
+  'hs': [/[\w|']+/, /(?:^|\s)--.+?$|\{-[\S\s]*?-\}/, null]
 }
 
 const escapeHTMLChar = function (ch) {
@@ -53,6 +53,18 @@ class ColorCache {
   }
 }
 
+const colorizeComment = function (cm) {
+  return `<span style="color: slategray">${escapeHTML(cm)}</span>`
+}
+
+const colorizeStrLit = function (cache, str) {
+  return `<span style="color: mediumslateblue">${escapeHTML(str)}</span>`
+}
+
+const colorizeWord = function (cache, word) {
+  return `<span style="color: #${cache.forWord(word)}">${word}</span>`
+}
+
 export default function (input, {
   language = null,
   colors = defaultColors
@@ -60,38 +72,24 @@ export default function (input, {
   const optionsAltered = languageOptions[language] || [null, null, null]
   const options = optionsAltered.map((x, id) => x || defaultLanguageOptions[id])
 
-  const cache = new ColorCache(colors)
-
   const wordRe = options[0]
   const commentRe = options[1]
   const stringLitRe = options[2]
   const escapeCharRe = /[&<"']/
 
   const re = new RegExp(`(${commentRe.source})|(${stringLitRe.source})|(${wordRe.source})|(${escapeCharRe.source})`, 'gmi')
-  console.log(re)
-  return input
-  .replace(re, (m, cm, str, word, esc) => {
+
+  const cache = new ColorCache(colors)
+
+  return input.replace(re, (_, cm, str, word, esc) => {
     if (cm) {
-      return toComment(cm)
-    }
-
-    if (str) {
-      return toStringLit(str)
-    }
-
-    if (esc) {
+      return colorizeComment(cm)
+    } else if (str) {
+      return colorizeStrLit(cache, str)
+    } else if (word) {
+      return colorizeWord(cache, word)
+    } else if (esc) {
       return escapeHTMLChar(esc)
     }
-
-    const out = `<span style="color: #${cache.forWord(word)}">${word}</span>`
-    return out
   })
-}
-
-function toComment(cm) {
-  return `<span style="color: slategray">${escapeHTML(cm)}</span>`
-}
-
-function toStringLit(cm) {
-  return `<span style="color: mediumslateblue">${escapeHTML(cm)}</span>`
 }
